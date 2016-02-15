c1862faa7c30e4ae2f83149619dbe6248f68a3a3
Paulo Faria <paulo.faria.rl@gmail.com>
2016-02-15 20:57:07 -0200

    version 0.2.0

---
+++ Package.swift
@@ -0,0 +1,8 @@
+import PackageDescription
+
+let package = Package(
+    name: "Stream",
+    dependencies: [
+        .Package(url: "https://github.com/Zewo/Data.git", majorVersion: 0, minor: 2)
+    ]
+)
